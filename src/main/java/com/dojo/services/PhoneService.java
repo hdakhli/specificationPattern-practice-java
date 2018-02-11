9511d0534793d89c83ae073d92037c76c45b0665
Hicham Dakhli <hicham.dakhli@axa.fr>
2018-02-11 11:17:55 +0100

    Solution to exo02

--- src/main/java/com/dojo/services/PhoneService.java
+++ src/main/java/com/dojo/services/PhoneService.java
@@ -2,6 +2,7 @@ package com.dojo.services;
 
 import com.dojo.domain.Phone;
 import com.dojo.domain.PhoneBrand;
+import com.dojo.domain.PhoneType;
 import com.dojo.repositories.IPhoneRepository;
 
 import javax.inject.Inject;
@@ -22,7 +23,7 @@ public class PhoneService implements IPhoneService {
      */
     public List<Phone> getAllPremiumPhones() {
         return phoneRepository.findAllPhones().stream()
-                .filter(phone -> phone.getCost() >= 400)
+                .filter(phone -> phone.getCost() >= 400 && !phone.getType().equals(PhoneType.BASIC))
                 .collect(Collectors.toList());
     }
 
@@ -34,7 +35,8 @@ public class PhoneService implements IPhoneService {
 
     public List<Phone> getPremiumSamsungPhones() {
         return phoneRepository.findAllPhones().stream()
-                .filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) && phone.getCost() >= 400)
+                .filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) && phone.getCost() >= 400
+                        && !phone.getType().equals(PhoneType.BASIC))
                 .collect(Collectors.toList());
     }
 
@@ -46,7 +48,9 @@ public class PhoneService implements IPhoneService {
 
     public List<Phone> getPremiumSamsungAndHTCPhones() {
         return phoneRepository.findAllPhones().stream()
-                .filter(phone -> (phone.getBrand().equals(PhoneBrand.SAMSUNG) || phone.getBrand().equals(PhoneBrand.HTC)) && phone.getCost() >= 400)
+                .filter(phone -> (phone.getBrand().equals(PhoneBrand.SAMSUNG)
+                        || phone.getBrand().equals(PhoneBrand.HTC))
+                        && phone.getCost() >= 400 && !phone.getType().equals(PhoneType.BASIC))
                 .collect(Collectors.toList());
     }
 
@@ -58,7 +62,9 @@ public class PhoneService implements IPhoneService {
 
     public List<Phone> getAllPremiumExceptSamsungAndHTCPhones() {
         return phoneRepository.findAllPhones().stream()
-                .filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG) && !phone.getBrand().equals(PhoneBrand.HTC) && phone.getCost() >= 400)
+                .filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG)
+                        && !phone.getBrand().equals(PhoneBrand.HTC) && phone.getCost() >= 400
+                        && !phone.getType().equals(PhoneType.BASIC))
                 .collect(Collectors.toList());
     }
 
