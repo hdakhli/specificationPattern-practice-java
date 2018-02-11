9b6738aee87c5621c6c01120a3c5e0174b9eea29
Hicham Dakhli <hicham.dakhli@axa.fr>
2018-02-11 17:28:34 +0100

    Solution to exo04

--- src/main/java/com/dojo/specifications/impl/PremiumPhoneSpecification.java
+++ src/main/java/com/dojo/specifications/impl/PremiumPhoneSpecification.java
@@ -1,12 +1,13 @@
 package com.dojo.specifications.impl;
 
 import com.dojo.domain.Phone;
+import com.dojo.domain.PhoneType;
 import com.dojo.specifications.pattern.AbstractCompositeSpecification;
 
 public class PremiumPhoneSpecification extends AbstractCompositeSpecification<Phone> {
 
     @Override
     public boolean isSatisfiedBy(Phone phone) {
-        return phone.getCost() >= 400;
+        return phone.getCost() >= 400 && !PhoneType.BASIC.equals(phone.getType());
     }
 }
