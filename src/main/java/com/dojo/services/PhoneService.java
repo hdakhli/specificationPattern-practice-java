ea66aa1455064a2626c42c0a5c92dc71330876ef
hdakhli <hichamdakhli@gmail.com>
2019-06-30 10:58:33 +0200

    Phone service formatting & import declaration

--- src/main/java/com/dojo/services/PhoneService.java
+++ src/main/java/com/dojo/services/PhoneService.java
@@ -1,10 +1,12 @@
 package com.dojo.services;
 
 import com.dojo.domain.Phone;
+import com.dojo.domain.PhoneBrand;
 import com.dojo.repositories.IPhoneRepository;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PhoneService implements IPhoneService {
 
@@ -19,31 +21,45 @@ public class PhoneService implements IPhoneService {
      * premium phones: cost >= 400
      */
     public List<Phone> getAllPremiumPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> phone.getCost() >= 400).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> phone.getCost() >= 400)
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getSamsungPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG)).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG))
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getPremiumSamsungPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) && phone.getCost() >= 400).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) && phone.getCost() >= 400)
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getSamsungAndHTCPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) || phone.getBrand().equals(PhoneBrand.HTC)).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> phone.getBrand().equals(PhoneBrand.SAMSUNG) || phone.getBrand().equals(PhoneBrand.HTC))
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getPremiumSamsungAndHTCPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> (phone.getBrand().equals(PhoneBrand.SAMSUNG) || phone.getBrand().equals(PhoneBrand.HTC)) && phone.getCost() >= 400).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> (phone.getBrand().equals(PhoneBrand.SAMSUNG) || phone.getBrand().equals(PhoneBrand.HTC)) && phone.getCost() >= 400)
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getAllExceptSamsungPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG)).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG))
+                .collect(Collectors.toList());
     }
 
     public List<Phone> getAllPremiumExceptSamsungAndHTCPhones() {
-        return phoneRepository.findAllPhones().stream().filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG) && !phone.getBrand().equals(PhoneBrand.HTC) && phone.getCost() >= 400).collect(Collectors.toList());
+        return phoneRepository.findAllPhones().stream()
+                .filter(phone -> !phone.getBrand().equals(PhoneBrand.SAMSUNG) && !phone.getBrand().equals(PhoneBrand.HTC) && phone.getCost() >= 400)
+                .collect(Collectors.toList());
     }
 
 }
